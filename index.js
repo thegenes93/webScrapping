db7d2af9b50e1f512ffb5a87f92212bfe1698edc
Theogenes <41095513+thegenes93@users.noreply.github.com>
2021-10-27 11:26:40 -0300

    ajustes erros node modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://covid.saude.gov.br/');
     await page.screenshot({ path: `${Date.now()}+covidsaudegovbr.png` });
